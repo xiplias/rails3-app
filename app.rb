8127b357d3acff2dab23715aed6425bbfb71ac4d
Matt Colyer <matt@colyer.name>
2010-09-25 11:07:07 -0700

    Remove script prefix from rails commands.

--- app.rb
+++ app.rb
@@ -58,9 +58,9 @@ Run the following commands to complete the setup of #{app_name.humanize}:
 
 % cd #{app_name}
 % bundle install
-% script/rails generate rspec:install
-% script/rails generate devise:install
-% script/rails generate devise MODEL
+% rails generate rspec:install
+% rails generate devise:install
+% rails generate devise MODEL
 DOCS
 
 log docs
